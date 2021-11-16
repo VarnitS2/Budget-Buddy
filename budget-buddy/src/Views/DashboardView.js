b23eead75dbd7ee599e308b434b28370b219ac37
Varnit <varnits02@gmail.com>
2021-11-15 23:55:22 -0600

    Update dashboard view

--- budget-buddy/src/Views/DashboardView.js
+++ budget-buddy/src/Views/DashboardView.js
@@ -2,15 +2,58 @@ import React, { useState, useEffect } from "react";
 import { Button, Typography } from "@material-ui/core";
 import { useNavigate } from "react-router-dom";
 import useStyles from "../style";
+import DateRangePicker from "../components/DateRangePicker";
+import BalanceLineChart from "../components/BalanceLineChart";
+import BalanceProgressBar from "../components/BalanceProgressBar";
 
 function DashboardView() {
   const classes = useStyles();
   const navigate = useNavigate();
 
   const [currentUser, setCurrentUser] = useState("");
-  const [transactions, setTransactions] = useState([]);
+  const [dates, setDates] = useState([null, null]);
+  const [lineChartData, setLineChartData] = useState([]);
+  const [paycheckPercent, setPaycheckPercent] = useState(100);
 
-  const [noTransactionsFlag, setNoTransactionsFlag] = useState(true);
+  useEffect(async () => {
+    if (window.sessionStorage.getItem("currentUser") === null) {
+      navigate("/login");
+    } else {
+      setCurrentUser(window.sessionStorage.getItem("currentUser"));
+
+      const requestOptions = {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({
+          email: window.sessionStorage.getItem("currentUser"),
+          start_date: "2020-12-31T",
+          end_date: "2021-12-31T",
+        }),
+      };
+
+      await fetch("/api/user/transaction/dates", requestOptions)
+        .then((response) => response.json())
+        .then((data) => {
+          setLineChartData(data.message);
+        });
+
+      const requestOptionsTwo = {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({
+          email: window.sessionStorage.getItem("currentUser"),
+        }),
+      };
+
+      await fetch("api/user/paycheck/percent", requestOptionsTwo)
+        .then((response) => response.json())
+        .then((data) => {
+          setPaycheckPercent(data.message);
+        });
+    }
+  }, []);
+
+  useEffect(() => {console.log(paycheckPercent)}, [paycheckPercent]);
 
   const handleAddTransactionButtonPressed = () => {
     navigate("/add-transaction");
@@ -25,63 +68,106 @@ function DashboardView() {
     navigate("/");
   };
 
-  useEffect(() => {
-    if (window.sessionStorage.getItem("currentUser") === null) {
-      navigate("/login");
-    } else {
-      setCurrentUser(window.sessionStorage.getItem("currentUser"));
+  const datesChangeCallback = (newDates) => {
+    setDates(newDates);
+  };
 
-      const requestOptions = {
-        method: "POST",
-        headers: { "Content-Type": "application/json" },
-        body: JSON.stringify({
-          email: currentUser,
-        }),
-      };
-
-      fetch("/api/user/get-transactions", requestOptions)
-        .then((response) => response.json())
-        .then((data) => {
-          if (data.status === 200) {
-            setNoTransactionsFlag(false);
-            setTransactions(data.data);
-          } else {
-            setNoTransactionsFlag(true);
-          }
-        });
+  const handleFetchDatesButtonPressed = async () => {
+    if (dates[0] === null || dates[1] === null) {
+      return;
     }
-  }, []);
+
+    const requestOptions = {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        email: currentUser,
+        start_date: dates[0],
+        end_date: dates[1],
+      }),
+    };
+
+    await fetch("/api/user/transaction/dates", requestOptions)
+      .then((response) => response.json())
+      .then((data) => {
+        setLineChartData(data.message);
+      });
+  };
 
   return (
-    <div className={classes.root}>
-      <div className={classes.displayCenter}>
-        <Typography className={classes.dashboardView_title}>Dashboard</Typography>
+    <div>
+      <div className={classes.pad}>
+        <div className={classes.dashboardView_topNavBar}>
+          <div>
+            <Button
+              className={classes.dashboardView_buttons__addTransaction}
+              variant="outlined"
+              onClick={handleAddTransactionButtonPressed}
+            >
+              Add transaction
+            </Button>
+          </div>
+
+          <Typography className={classes.dashboardView_title}>
+            DASHBOARD
+          </Typography>
+
+          <div>
+            <Button
+              className={classes.dashboardView_buttons__userSettings}
+              variant="outlined"
+              onClick={handleUserSettingsButtonPressed}
+            >
+              User Settings
+            </Button>
+
+            <Button
+              className={classes.dashboardView_buttons__signOut}
+              variant="outlined"
+              onClick={handleSignOutButtonPressed}
+            >
+              Sign Out
+            </Button>
+          </div>
+        </div>
       </div>
 
-      <div className={classes.displayCenter}>
-        <Button
-          className={classes.dashboardView_buttons}
-          variant="outlined"
-          onClick={handleAddTransactionButtonPressed}
-        >
-          Add transaction
-        </Button>
+      <div className={classes.dashboardView_midRowContainer}>
+        <div className={classes.dashboardView_transactionsContainer}>
+          <BalanceLineChart data={lineChartData} />
+        </div>
 
-        <Button
-          className={classes.dashboardView_buttons}
-          variant="outlined"
-          onClick={handleUserSettingsButtonPressed}
-        >
-          User Settings
-        </Button>
+        <div className={classes.dashboardView_dateRangeContainer}>
+          <Typography className={classes.dashboardView_dateRangeTitle}>
+            FETCH TRANSACTIONS BETWEEN A DATE RANGE
+          </Typography>
 
-        <Button
-          className={classes.dashboardView_buttons}
-          variant="outlined"
-          onClick={handleSignOutButtonPressed}
-        >
-          Sign Out
-        </Button>
+          <div className={classes.dashboardView_dateRangeInner}>
+            <div className={classes.dashboardView_dateRangeInnerInner}>
+              <DateRangePicker datesChangeCallback={datesChangeCallback} />
+            </div>
+          </div>
+
+          <Button
+            className={classes.dashboardView_dateRangeFetchButton}
+            variant="outlined"
+            onClick={handleFetchDatesButtonPressed}
+          >
+            FETCH
+          </Button>
+        </div>
+      </div>
+
+      <div className={classes.pad}>
+        <div className={classes.dashboardView_bottomRowContainer}>
+          <div className={classes.displayColumn}>
+            <BalanceProgressBar percent={paycheckPercent} />
+
+            <Typography className={classes.dashboardView_lastPaycheckText}>
+              LAST PAYCHECK
+            </Typography>
+          </div>
+        </div>
       </div>
     </div>
   );
